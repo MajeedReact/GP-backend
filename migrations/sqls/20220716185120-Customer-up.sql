26e98ed18745dc9a6148fac59d83836150299706
MajeedReact <mjalsalhi@gmail.com>
2022-09-13 21:51:16 +0300

    Removed insert query

--- migrations/sqls/20220716185120-Customer-up.sql
+++ migrations/sqls/20220716185120-Customer-up.sql
@@ -1,4 +1,3 @@
 /* Replace with your SQL commands */
 CREATE TABLE Customer(Customer_id SERIAL PRIMARY KEY,Customer_Email VARCHAR,Cus_First_Name text,Cus_Last_Name VARCHAR(15),Customer_Password text, Role_id BIGINT REFERENCES Roles(Role_id));
 
-insert INTO Customer VALUES (1,'1234@gmail.com','Abdullah','aldaheri','a1234',1);
