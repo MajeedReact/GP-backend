0e80b012cd9d7f606f607685389f0e78010e87ec
Abdullah Aldaheri <abdullahaldaherii1999@gmail.com>
2022-09-28 08:43:09 +0300

    add constraint to personal information

--- migrations/sqls/20220716183259-Seller-up.sql
+++ migrations/sqls/20220716183259-Seller-up.sql
@@ -1,2 +1,2 @@
 /* Replace with your SQL commands */
-CREATE TABLE Seller(Seller_id SERIAL PRIMARY KEY,Seller_Email VARCHAR,created_at date not null default CURRENT_DATE, Seller_Password text,Shop_Name VARCHAR(30),Role_id BIGINT REFERENCES Roles(Role_id));
+CREATE TABLE Seller(Seller_id SERIAL PRIMARY KEY,Seller_Email VARCHAR NOT NULL,created_at date not null default CURRENT_DATE, Seller_Password text,Shop_Name VARCHAR(30) NOT NULL,Role_id BIGINT REFERENCES Roles(Role_id));
